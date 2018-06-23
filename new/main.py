9d5abf06755f3811c21dfcf53fe0ffcda835780f
ALoserIsMe <32465750+ALoserIsMe@users.noreply.github.com>
2018-06-23 10:51:33 +0100

    Update main.py
    changed the random number section to include a -1, as the previous version would often return errors

--- new/main.py
+++ new/main.py
@@ -15,22 +15,22 @@ def SentenceGenerate():
   verbs = ["VerbExample1", "VerbExample2"]
   propositions = ["on", "in", "at", "since", "for", "ago", "before", "to", "past", "by"]
   
-  articleInt = random.randint(0,len(articles))
+  articleInt = random.randint(0,len(articles)-1)
   sentenceArr[0] =  articles[articleInt]
   str(sentenceArr[0])
-  nounInt = random.randint(0,len(nouns))
+  nounInt = random.randint(0,len(nouns)-1)
   sentenceArr[1] = nouns[nounInt]
   str(sentenceArr[1])
-  verbInt = random.randint(0,len(verbs))
+  verbInt = random.randint(0,len(verbs)-1)
   sentenceArr[2] = verbs[verbInt]
   str(sentenceArr[2])
-  propositionInt = random.randint(0,len(propositions))
+  propositionInt = random.randint(0,len(propositions)-1)
   sentenceArr[3] = propositions[propositionInt]
   str(sentenceArr[3])
-  articleInt = random.randint(0,len(articles))
+  articleInt = random.randint(0,len(articles)-1)
   sentenceArr[4] = articles[articleInt]
   str(sentenceArr[4])
-  nounInt = random.randint(0,len(nouns))
+  nounInt = random.randint(0,len(nouns)-1)
   sentenceArr[5] = nouns[nounInt]
   str(sentenceArr[5])
   
